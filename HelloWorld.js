09bd523b8eafd32b9e10c62a65b9dcdbf5e82662
SMurtazaRizvi <muhammadmurtaza535@gmail.com>
2019-01-18 23:53:33 +0500

    My First commit

---
+++ HelloWorld.js
@@ -0,0 +1,10 @@
+var startup = /** @class */ (function () {
+    function startup() {
+    }
+    startup.main = function () {
+        console.log('hello world');
+        return 0;
+    };
+    return startup;
+}());
+startup.main();
---
+++ HelloWorld.ts
@@ -0,0 +1,7 @@
+class startup {
+    public static main(): number {
+        console.log('hello world');
+        return 0;
+    }
+}
+startup.main();
